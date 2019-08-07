a5418fdd9ab259347efc30d4984d90e5728bee49
Aditya Kumar Jha <adityajha126@gmail.com>
2019-08-07 11:37:42 +0530

    first program in java

---
+++ firstPattern.java
@@ -0,0 +1,36 @@
+public class firstPattern
+{
+public static void main(String[] args) {
+System.out.println("print shape");
+int draw = 5;
+switch (draw) {
+  case 1:
+    System.out.println("|");
+    break;
+  case 2:
+    System.out.println("|/n");
+    System.out.println("||/n");
+    break;
+  case 3:
+    System.out.println("|");
+    System.out.println("||");
+    System.out.println("|||");
+    break;
+  case 4:
+     System.out.println(" |   ");
+     System.out.println("||");
+     System.out.println("|||");
+     System.out.println("||||");
+   
+  case 5:
+    System.out.println("|");
+    System.out.println("||");
+    System.out.println("|||");
+    System.out.println("||||");
+    System.out.println("|||||");
+    
+    break;
+  
+}
+}
+}
